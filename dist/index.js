8a95ac8fe0bd0fe7f917bf78d37bed7a576e2d41
hamdi <hamdi.pirillo4@gmail.com>
2024-12-08 11:15:05 +0100

    learned to process streams and send responses to the client

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const http_1 = require("http");
-const server = (0, http_1.createServer)();
-server.listen(3000, 'localhost', () => console.log('The server is up and running'));
-server.on('request', (req, res) => {
-    (0, fs_1.createReadStream)('package.json')
-        .on('data', chunk => res.write(chunk))
-        .on('end', () => res.end());
-});
+connect(3000);
+function connect(port) {
+    const server = (0, http_1.createServer)();
+    server.listen(port, 'localhost', () => console.log('The server is running on port:', port));
+    server.on('request', (req, res) => {
+        req.setEncoding('utf-8');
+        req
+            .on('readable', () => {
+            let path;
+            while ((path = req.read()) !== null) {
+                (0, fs_1.createReadStream)(path)
+                    .on('error', (err) => {
+                    if (err.code === 'ENOENT') {
+                        res.end('No such file exists.');
+                        return;
+                    }
+                    console.error(err);
+                })
+                    .pipe(res);
+            }
+        });
+    });
+}
--- index.ts
+++ index.ts
@@ -5,11 +5,29 @@ connect(3000)
 
 function connect(port: number) {
     const server = createServer()
-    server.listen(port, 'localhost', () => console.log('The server is up and running'))
+    server.listen(port, 'localhost', () => console.log('The server is running on port:', port))
 
     server.on('request', (req, res) => {
-        createReadStream('package.json')
-            .on('data', chunk => res.write(chunk))
-            .on('end', () => res.end())
+        req.setEncoding('utf-8')
+
+        req
+            .on('readable', () => {
+                let path
+
+                while ((path = req.read()) !== null) {
+                    createReadStream(path)
+                        .pipe(res)
+                        .on('error', handleError)
+                }
+            })
+
+        function handleError(err: any) {
+            if (err.code === 'ENOENT') {
+                res.end('No such file exists.')
+                return
+            }
+
+            console.error(err)
+        }
     })
 }
